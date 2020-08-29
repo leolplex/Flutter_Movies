65f6f0e5f4821009c5718972cfda306fe391cf63
Daniel <raiga1234@gmail.com>
2020-08-28 22:20:30 -0500

    Refactor cardswiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:movies/src/widgets/card_swiper_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -24,23 +23,8 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swipeCards() {
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity,
-      height: 300.0,
-      child: Swiper(
-        itemBuilder: (BuildContext context, int index) {
-          return new Image.network(
-            "http://via.placeholder.com/350x150",
-            fit: BoxFit.fill,
-          );
-        },
-        itemCount: 3,
-        pagination: new SwiperPagination(),
-        control: new SwiperControl(),
-        layout: SwiperLayout.STACK,
-        itemWidth: 300.0,
-      ),
+    return CardSwiper(
+      movies: [1, 2, 3, 4, 5],
     );
   }
 }

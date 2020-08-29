187bf9e17fc21671f386320a69d18dd465a7649f
Daniel <raiga1234@gmail.com>
2020-08-28 22:02:06 -0500

    Implement Swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Movies at cinemas'),
+          backgroundColor: Colors.indigoAccent,
+          actions: <Widget>[
+            IconButton(icon: Icon(Icons.search), onPressed: () {})
+          ],
+        ),
         body: SafeArea(
-      child: Text('Holaaaaaaaaaa'),
-    ));
+          child: Column(
+            children: <Widget>[_swipeCards()],
+          ),
+        ));
+  }
+
+  Widget _swipeCards() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return new Image.network(
+            "http://via.placeholder.com/350x150",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        pagination: new SwiperPagination(),
+        control: new SwiperControl(),
+        layout: SwiperLayout.STACK,
+        itemWidth: 300.0,
+      ),
+    );
   }
 }

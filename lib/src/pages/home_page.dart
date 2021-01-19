46c12a1ed15b947f18b9fc2b65fd4544c9d5ebf5
Daniel <raiga1234@gmail.com>
2021-01-19 12:41:41 -0500

    Add movies horizontal widget

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies/src/providers/movies_provider.dart';
 import 'package:movies/src/widgets/card_swiper_widget.dart';
+import 'package:movies/src/widgets/movies_horizontal.dart';
 
 class HomePage extends StatelessWidget {
   final moviesProvider = new MoviesProvider();
@@ -44,25 +45,27 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _footer(BuildContext context) {
-    final _screenSize = MediaQuery.of(context).size;
     return Container(
       width: double.infinity,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Text("Populares", style: Theme.of(context).textTheme.subtitle1),
+          Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text("Populares",
+                  style: Theme.of(context).textTheme.subtitle1)),
+          SizedBox(
+            height: 5.0,
+          ),
           FutureBuilder(
             future: moviesProvider.getPopulars(),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (snapshot.hasData) {
-                return CardSwiper(
+                return MovieHorizontal(
                   movies: snapshot.data,
-                  width: _screenSize.width * 0.8, // 70%
-                  height: _screenSize.height * 0.3, // 50%
                 );
               } else {
-                return Container(
-                    height: 400.0,
-                    child: Center(child: CircularProgressIndicator()));
+                return Center(child: CircularProgressIndicator());
               }
             },
           ),

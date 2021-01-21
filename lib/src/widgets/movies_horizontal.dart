21507b03274911760e9e5839e43ad56e814b71d3
Daniel <raiga1234@gmail.com>
2021-01-20 19:00:11 -0500

    Optimize code

--- lib/src/widgets/movies_horizontal.dart
+++ lib/src/widgets/movies_horizontal.dart
@@ -22,10 +22,38 @@ class MovieHorizontal extends StatelessWidget {
 
     return Container(
       height: _screenSize.height * 0.2,
-      child: PageView(
+      child: PageView.builder(
         pageSnapping: false,
         controller: _pageController,
-        children: _cards(context),
+        itemCount: movies.length,
+        itemBuilder: (context, i) => _card(context, movies[i]),
+      ),
+    );
+  }
+
+  Widget _card(BuildContext context, Movie movie) {
+    return Container(
+      margin: EdgeInsets.only(right: 15.0),
+      child: Column(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              image: NetworkImage(movie.getPosterImage()),
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              fit: BoxFit.cover,
+              height: 140.0,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            movie.title,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.caption,
+          )
+        ],
       ),
     );
   }
